23803d06d8f2e8655c814dbb6dcc0bdfaf2247a1
digvijaybhakuni <digvijay.bhakuni@tetruscorp.com>
2021-05-11 16:17:19 +0530

    useContext API

--- the-blog/src/App.js
+++ the-blog/src/App.js
@@ -1,31 +1,39 @@
 import './App.css';
 
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
-import { Suspense } from 'react';
+import React, { Suspense, useContext, useState } from 'react';
 import { Blog } from './Blog';
-import { post } from 'jquery';
 import { Post } from './Post';
 import { Author } from './Author';
+import { Login } from './Login';
+import { Loading } from './Loading';
+import { LoginContext } from './Context';
+
 
 function App() {
-  const sampleStr = "==="
+  const [isLogin, setIsLogin] = useState(false);
+  const [username, setUsername] = useState();
+
   return (
     <div className="App">
       <BrowserRouter>
-        <header>
-          <Headers />
-        </header>
-        <main role="main" className="flex-shrink-0">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/blog" component={Blog} />
-              <Route exact path="/post/:postId" component={Post} />
-              <Route exact path="/author/:authorId" component={Author} />
-              <Route path="/about" component={About} />
-            </Switch>
-          </Suspense>
-        </main>
+        ̥<LoginContext.Provider value={{ isLogin, setIsLogin, username, setUsername }}>
+          <header>
+            <Headers />
+          </header>
+          <main role="main" className="flex-shrink-0">
+            <Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/blog" component={Blog} />
+                <Route exact path="/post/:postId" component={Post} />
+                <Route exact path="/author/:authorId" component={Author} />
+                <Route path="/about" component={About} />
+              </Switch>
+            </Suspense>
+          </main>
+        </LoginContext.Provider>
       </BrowserRouter>
       <Footer />
     </div>
@@ -33,16 +41,16 @@ function App() {
 }
 
 
-const noop = () => {};
+const noop = () => { };
 
 const Headers = () => {
+  const { isLogin, username } = useContext(LoginContext);
   return <div className="container">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/blog">theBlog</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
@@ -52,8 +60,8 @@ const Headers = () => {
             <Link className="nav-link" to="/about">About</Link>
           </li>
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" id="navbarDropdown" 
-              href="#" role="button" data-toggle="dropdown" 
+            <a className="nav-link dropdown-toggle" id="navbarDropdown"
+              href="#" role="button" data-toggle="dropdown"
               aria-haspopup="true" aria-expanded="false">Dropdown</a>
             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
               <a className="dropdown-item" href="#">Action</a>
@@ -70,6 +78,11 @@ const Headers = () => {
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+        <span>
+          {!isLogin ?
+            <Link to="/login">Login</Link> : <span>{username}</span>
+          }
+        </span>
       </div>
     </nav>
   </div>
@@ -86,7 +99,7 @@ const Footer = () => {
 const Home = () => {
   return <div className="container">
     <Jumbotron />
-    <Blog/>
+    <Blog />
   </div>
 }
 
--- the-blog/src/Post.js
+++ the-blog/src/Post.js
@@ -21,10 +21,29 @@ export const Post = (props) => {
     return <>
         <div className="container">
             <div className="main-post">
-                {post && JSON.stringify(post)}
+                {post && <PostMain post={post}/> }
             </div>
             { loading && <Loading />}
         </div>
     </>;
 
 };
+
+
+const PostMain = ({post}) => <div>
+    <div className="postMain">
+        <div className="title">{post.title}</div>
+        <div className="author-n-tags">
+            <div className="author-name-link">DigvijayB</div>
+            <div className="tags">
+                <span>java</span>
+                <span>spring</span>
+                <span>programing</span>
+            </div>
+        </div>
+        <div className="date_post">2021/04/01</div>
+        <div className="post_content">
+            {post.body}
+        </div>
+    </div>
+</div>
