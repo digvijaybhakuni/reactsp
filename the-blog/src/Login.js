4b3aae355700ec37b1fc1b12e91f0298611e1372
digvijaybhakuni <digvijay.bhakuni@tetruscorp.com>
2021-05-19 22:32:33 +0530

    login changes

--- the-blog/src/Login.js
+++ the-blog/src/Login.js
@@ -1,59 +1,66 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer, useState } from "react";
 import { LoginContext } from "./Context";
 import "./Login.css";
+
+const loginReducer = (state, action) => {
+    switch (action.type) {
+        case 'login':
+            if (state.username === 'dg90@gmail.com' && state.password === 'password') {
+                return { ...state, isLogin: true };
+                // setIsLogin(true);
+                // setUsername("DG90");
+            } else {
+                // setIsLogin(false);
+                // setUsername(null);
+                return { ...state, isLogin: false };
+            }
+        case 'field':
+            return { ...state, [action.field]: action.payload }
+        default:
+            return { ...state };
+    }
+};
+
 export const Login = (props) => {
 
-    const {setIsLogin, setUsername} = useContext(LoginContext);
+    const { setIsLogin, setUsername } = useContext(LoginContext);
+
+    const [state, dispatch] = useReducer(loginReducer, {isLogin: false, username: '', password: '', remenberMe: 'off'});
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [remenberMe, setRemenberMe] = useState();
 
     const doLogin = (ev) => {
         console.log('ev', ev);
-        setIsLogin(true);
-        setUsername("DG90");
     };
 
     return <div className="container">
         <div className="login_form_container">
             <form>
-                <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" />
+                <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                        <input type="email" onChange={(ev) => setEmail(ev.target.value)} className="form-control" id="inputEmail3" />
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3" />
+                <div className="form-group row">
+                    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" onChange={(ev) => setPassword(ev.target.value)} className="form-control" id="inputPassword3" />
                     </div>
                 </div>
-                <fieldset class="form-group row">
-                    <legend class="col-form-label col-sm-2 float-sm-left pt-0">Radios</legend>
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked />
-                            <label class="form-check-label" for="gridRadios1"> First radio </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2" />
-                            <label class="form-check-label" for="gridRadios2"> Second radio </label>
-                        </div>
-                        <div class="form-check disabled">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled />
-                            <label class="form-check-label" for="gridRadios3"> Third disabled radio </label>
+                <div className="form-group row">
+                    <div className="col-sm-10 offset-sm-2">
+                        <div className="form-check">
+                            <input className="form-check-input" onChange={(ev) => setRemenberMe(ev.target.value)} type="checkbox" id="gridCheck1" />
+                            <label className="form-check-label" for="gridCheck1">Remember Me</label>
                         </div>
                     </div>
-                </fieldset>
-                <div class="form-group row">
-                    <div class="col-sm-10 offset-sm-2">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck1" />
-                            <label class="form-check-label" for="gridCheck1">Example checkbox</label>
+                    <div className="form-group row">
+                        <div className="col-sm-10">
+                            <button type="button" onClick={doLogin} className="btn btn-primary">Sign in</button>
                         </div>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-10">
-                        <button type="button" onClick={doLogin} class="btn btn-primary">Sign in</button>
                     </div>
                 </div>
             </form>
